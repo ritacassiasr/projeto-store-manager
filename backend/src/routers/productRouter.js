a5008993751556a72ca2861b7cea097e8dadfda6
ritacassiasr@gmail.com <ritacassiasr@gmail.com>
2023-10-26 16:30:35 -0400

    feat: criando validações na criação de produtos

--- backend/src/routers/productRouter.js
+++ backend/src/routers/productRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const productController = require('../controllers/productController');
+const { validatedName } = require('../middlewares/validateProduct');
 
 router.get('/', productController.getAll);
 router.get('/:id', productController.getById);
-router.post('/', productController.create);
+router.post('/', validatedName, productController.create);
 
 module.exports = router;
