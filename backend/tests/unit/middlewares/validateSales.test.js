3f3dd64d61d430aeda978f9ee05c2dae187aa038
ritacassiasr@gmail.com <ritacassiasr@gmail.com>
2023-10-26 16:53:17 -0400

    test: teste unitário para validação na criação de nova venda

---
+++ backend/tests/unit/middlewares/validateSales.test.js
@@ -0,0 +1,85 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const {
+  validateProductId,
+  saleQuantity,
+} = require('../../../src/middlewares/validateSales');
+
+describe('Testando middlewares sale', function () {
+  it('Ao terminar a verificação, o next é chamado', async function () {
+    const req = {
+      body: [
+        {
+          productId: 1,
+          quantity: 1,
+        },
+        {
+          productId: 2,
+          quantity: 5,
+        },
+      ],
+    };
+    const res = {};
+    const next = sinon.stub().returns();
+
+    validateProductId(req, res, next);
+    saleQuantity(req, res, next);
+
+    expect(next).to.have.been.calledWith();
+  });
+  it('testando sale sem o campo id do produto', async function () {
+    const req = {
+      body: [
+        {
+          quantity: 1,
+        },
+        {
+          productId: 3,
+          quantity: 10,
+        },
+      ],
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    const next = sinon.stub().returnsThis();
+
+    validateProductId(req, res, next);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({
+      message: '"productId" is required',
+    });
+  });
+  it('testando sale sem o campo quantity do produto', async function () {
+    const req = {
+      body: [
+        {
+          productId: 2,
+        },
+        {
+          productId: 3,
+          quantity: 10,
+        },
+      ],
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    const next = sinon.stub().returnsThis();
+
+    saleQuantity(req, res, next);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({
+      message: '"quantity" is required',
+    });
+  });
+});
